3342dba8ffd9b2f8f20c99f5da792fff6767f703
Dmytro Dakhnenko <dmytro.dakhnenko@gmail.com>
2020-02-12 16:26:16 +0200

    first commit

---
+++ src/app/data/users.js
@@ -0,0 +1,24 @@
+{
+  "users": [
+    {
+      "id": 1,
+      "name": "Adam",
+      "email": "adam@email.com"
+    },
+    {
+      "id": 2,
+      "name": "Peter",
+      "email": "peter@email.com"
+    },
+    {
+      "id": 3,
+      "name": "David",
+      "email": "david@email.com"
+    },
+    {
+      "id": 4,
+      "name": "Alex",
+      "email": "alex@email.com"
+    }
+  ]
+}
---
+++ src/app/else/else.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './else.component.html',
+  styleUrls: ['./else.component.css']
+})
+export class ElseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
